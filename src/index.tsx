fead7d92483af1a61c8dad22474d20fbfe88896c
Nicholas Galea <nich.galea@gmail.com>
2019-04-13 20:32:20 +0200

    Persist to localStorage

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
-import store from "./redux/store";
-import App from "./app";
+import { Joke } from "./models";
 
+import createStore from "./redux/store";
+import App from "./app";
 import "./styles.scss";
+
+const storageKey = "favourite-chuckles";
+
+const favouritesRaw = localStorage.getItem(storageKey);
+
+let store: ReturnType<typeof createStore>;
+
+if (favouritesRaw) {
+  store = createStore({ favourites: JSON.parse(favouritesRaw) });
+} else {
+  store = createStore();
+}
+
+store.subscribe(() => {
+  localStorage.setItem(storageKey, JSON.stringify(store.getState().favourites));
+});
 
 ReactDOM.render(
   <Provider store={store}>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,9 +2,12 @@ import { createStore } from "redux";
 
 import { favouritesReducer, FavouritesState } from "./favourites";
 
-export default createStore(
-  favouritesReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-);
-
 export type RootState = FavouritesState;
+
+export default (previousState?: RootState) => {
+  return createStore(
+    favouritesReducer,
+    previousState,
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+  );
+};
